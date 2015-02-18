0a7aaba57102c7ca5e2f9a00a8de3e270267b63e
Kurt Mohler <kurtamohler@gmail.com>
2015-02-17 20:42:03 -0500

    First commit

---
+++ Arduino/Sensors.h
@@ -0,0 +1,89 @@
+#if !defined SENSORS_h
+#define SENSORS_h
+
+// Class used to control analog mux that expands the amount
+// of analog inputs the system can use.
+class AnalogMux
+{
+public:
+    void SetSelect(uint8_t select);
+private:
+    uint8_t analogPin;
+    uint8_t selPin[3];
+};
+
+// Class used to obtain measurements from analog signals.
+// Update() must be run many times per second at a fairly
+// constant rate because it uses a first order lag filter to
+// minimize jitter.
+class AnalogSensor
+{
+public:
+    AnalogSensor(uint8_t pin, AnalogMux* mux, uint8_t muxSel);
+
+    // Obtain and return new filtered sample value.
+    uint8_t Update();
+
+private:
+    uint8_t pin;
+    AnalogMux* mux;
+    uint8_t muxSel;
+
+    uint8_t last;
+    unsigned long lastUpdateTimeUs;
+
+};
+
+class WindSensor
+{
+public:
+    WindSensor(uint8_t rvPin,  AnalogMux* rvMux,  uint8_t rvMuxSel,
+               uint8_t tmpPin, AnalogMux* tmpMux, uint8_t tmpMuxSel);
+
+    // Obtain and return new filtered sample value.
+    uint8_t Update();
+}
+
+// Controls a vent damper.
+class VentDamper
+{
+public:
+    VentDamper(uint8_t pin);
+
+    // Returns true if open, false otherwise.
+    bool IsOpen();
+
+    void Open();
+
+    void Close();
+};
+
+// Controls a door.
+class Door
+{
+    Door(uint8_t pin);
+
+    // Returns true if open, false otherwise.
+    bool IsOpen();
+
+    void Open();
+
+    void Close();
+};
+
+// Controls drapes.
+class Drapes
+{
+    Drapes(uint8_t pin);
+
+    // Returns true if open, false otherwise.
+    bool IsOpen();
+
+    void Open();
+
+    void Close();
+};
+
+
+
+#endif
