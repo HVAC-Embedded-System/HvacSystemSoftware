1740fdf937b49c6f534838bd2ba1871d86a08c98
eglerjl <eglerjl@gmail.com>
2015-04-01 20:48:18 -0400

    moved name change program

--- Arduino/Bluetooth/BTNameChange.ino
+++ Arduino/Bluetooth/BTNameChange.ino
@@ -5,14 +5,17 @@ SoftwareSerial bluetooth(10, 11); // RX, TX
 void setup()  
 {
  // Open serial communications and wait for port to open:
-  Serial.begin(9600)
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
 
   
-  Serial.println("Reply");
+  Serial.println("Goodnight moon!");
 
   // set the data rate for the SoftwareSerial port
   bluetooth.begin(9600);
-  bluetooth.println("Test msg");
+  bluetooth.println("Hello, world?");
 }
 
 void loop() // run over and over
